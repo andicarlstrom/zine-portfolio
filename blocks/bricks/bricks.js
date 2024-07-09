553fa5a60cf46f542c1d72b9b2bf0eaf53357505
Andi Carlstrom <andi.carlstrom@gmail.com>
2024-07-09 08:23:55 -0600

    Added brick component.

---
+++ blocks/bricks/bricks.js
@@ -0,0 +1,19 @@
+export default function decorate(block) {
+    const bricks = [...block.firstElementChild.children];
+    block.classList.add(`bricks-${bricks.length}-brks`);
+  
+    // setup image brick
+    [...block.children].forEach((row) => {
+      [...row.children].forEach((brk) => {
+        const pic = brk.querySelector('picture');
+        if (pic) {
+          const picWrapper = pic.closest('div');
+          if (picWrapper && picWrapper.children.length === 1) {
+            // picture is only content in brick
+            picWrapper.classList.add('bricks-img-brk');
+          }
+        }
+      });
+    });
+  }
+  
